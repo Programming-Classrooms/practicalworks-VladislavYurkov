0bbd2c3bf6db96400b1100cab398477f49859709
VladislavYurkov <dostupnii579@gmail.com>
2024-04-25 09:28:23 +0300

    Перенёс определения функций в cpp файл

--- laboratory-task-6.2-sem-2/src/Functions/Functions.cpp
+++ laboratory-task-6.2-sem-2/src/Functions/Functions.cpp
@@ -1,2 +1,178 @@
 #include "Functions.hpp"
 
+
+bool isInt(std::string word)
+{
+    if ((word[0] > '9' || word[0] < '0') && word[0] != '-') {
+        return false;
+    }
+    size_t wordLen = word.length();
+    for (size_t i = 0; i < wordLen; ++i) {
+        if (word[i] > '9' || word[i] < '0') {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::vector<int> readInt()
+{
+    std::vector<int> readedVector;
+
+    std::cout << "Input integers: ";
+    std::string line;
+    getline(std::cin, line);
+
+    std::string::size_type begInd;
+    std::string::size_type endInd;
+
+    begInd = line.find_first_not_of(" ");
+
+    while (begInd != std::string::npos) {
+        endInd = line.find_first_of(" ", begInd);
+        if (endInd == std::string::npos) {
+            endInd = line.length();
+        }
+        
+        std::string word = line.substr(begInd, endInd - begInd);
+
+        if (!isInt(word)) {
+            throw std::logic_error("Line has not only integers!");
+        }
+        
+        readedVector.push_back(stoi(word));
+
+        begInd = line.find_first_not_of(" ", endInd);
+    }
+
+    return readedVector;
+}
+
+int numbersSum(std::vector<int> rhs)
+{
+    int sum = 0;
+    for (auto i : rhs) {
+        sum += i;
+    }
+    return sum;
+}
+
+size_t numbersCount(std::vector<int> rhs)
+{
+    return rhs.size();
+}
+
+int inputInt()
+{
+    std::string line;
+    std::getline(std::cin, line);
+    if (!isInt(line)) {
+        throw std::logic_error("Inputed line is not a integer!");
+    }
+
+    return stoi(line);
+}
+
+size_t eqNumbersCount(std::vector<int> rhs, int target)
+{
+    size_t counter = 0;
+
+    for (auto i : rhs) {
+        if (i == target) {
+            ++counter;
+        }
+    }
+
+    return counter;
+}
+
+bool greaterThen(int lhs, int rhs)
+{
+    return lhs > rhs;
+}
+
+size_t suiteNumbersCount(std::vector<int> rhs, bool comp(int, int), int target)
+{
+    size_t counter = 0;
+    for (auto i : rhs) {
+        if (comp(i, target)) {
+            ++counter;
+        }
+    }
+    return counter;
+}
+
+void replaceNulls(std::vector<int>& rhs)
+{
+    int average = numbersSum(rhs) / numbersCount(rhs);
+    for (auto& i : rhs) {
+        if (i == 0) {
+            i = average;
+        }
+    }
+}
+
+void intervalInput(size_t& begInd, size_t& endInd)
+{
+    std::cout << "Input begin index: ";
+    int temp = inputInt();
+    if (temp < 0) {
+        throw std::out_of_range("Negative index is not allowed!");
+    }
+    begInd = static_cast<size_t>(temp);
+    std::cout << "Input end index: ";
+    temp = inputInt();
+    if (temp < 0) {
+        throw std::out_of_range("Negative index is not allowed!");
+    }
+    endInd = static_cast<size_t>(temp);
+    if (begInd > endInd) {
+        throw std::logic_error("Begin index bigger than end index!");
+    }
+}
+
+void increaceByRange(std::vector<int>& rhs)
+{
+    size_t begInd = 0;
+    size_t endInd = 0;
+    intervalInput(begInd, endInd);
+    std::vector<int> range(rhs.begin() + begInd, rhs.begin() + endInd + 1);
+    int increaceValue = numbersSum(range);
+    for (auto& elem : rhs) {
+        elem += increaceValue;
+    }
+}
+
+void replaceEven(std::vector<int>& rhs)
+{
+    int min = INT_MAX;
+    int max = INT_MIN;
+
+    for (auto elem : rhs) {
+        if (elem > max) {
+            max = elem;
+        }
+        if (elem < min) {
+            min = elem;
+        }
+    }
+    
+    int minMax = max - min;
+
+    for (auto& elem : rhs) {
+        if (elem % 2 == 0) {
+            elem = minMax;
+        }
+    }
+}
+
+void deleteRepeat(std::vector<int>& rhs)
+{
+    for (size_t i = 0; i < rhs.size() - 1; ++i) {
+        for (size_t j = i+1; j < rhs.size(); ++j) {
+            if (rhs[i] == rhs[j]) {
+                rhs.erase(rhs.begin() + j);
+            }
+        }
+    }
+}
--- laboratory-task-6.2-sem-2/src/Functions/Functions.hpp
+++ laboratory-task-6.2-sem-2/src/Functions/Functions.hpp
@@ -5,53 +5,9 @@
 #include <string>
 #include <climits>
 
+bool isInt(std::string);
 
-bool isInt(std::string word)
-{
-    if ((word[0] > '9' || word[0] < '0') && word[0] != '-') {
-        return false;
-    }
-    size_t wordLen = word.length();
-    for (size_t i = 0; i < wordLen; ++i) {
-        if (word[i] > '9' || word[i] < '0') {
-            return false;
-        }
-    }
-    return true;
-}
-
-std::vector<int> readInt()
-{
-    std::vector<int> readedVector;
-
-    std::cout << "Input integers: ";
-    std::string line;
-    getline(std::cin, line);
-
-    std::string::size_type begInd;
-    std::string::size_type endInd;
-
-    begInd = line.find_first_not_of(" ");
-
-    while (begInd != std::string::npos) {
-        endInd = line.find_first_of(" ", begInd);
-        if (endInd == std::string::npos) {
-            endInd = line.length();
-        }
-        
-        std::string word = line.substr(begInd, endInd - begInd);
-
-        if (!isInt(word)) {
-            throw std::logic_error("Line has not only integers!");
-        }
-        
-        readedVector.push_back(stoi(word));
-
-        begInd = line.find_first_not_of(" ", endInd);
-    }
-
-    return readedVector;
-}
+std::vector<int> readInt();
 
 template<typename T>
 void printVector(std::vector<T> rhs) 
@@ -63,133 +19,26 @@ void printVector(std::vector<T> rhs)
     std::cout << '\n';
 }
 
-int numbersSum(std::vector<int> rhs) 
-{
-    int sum = 0;
-    for (auto i : rhs) {
-        sum += i;
-    }
-    return sum;
-}
+int numbersSum(std::vector<int>);
 
-size_t numbersCount(std::vector<int> rhs)
-{
-    return rhs.size();
-}
+size_t numbersCount(std::vector<int>);
 
-int inputInt()
-{
-    std::string line;
-    std::getline(std::cin, line);
-    if (!isInt(line)) {
-        throw std::logic_error("Inputed line is not a integer!");
-    }
+int inputInt();
 
-    return stoi(line);
-}
+size_t eqNumbersCount(std::vector<int>, int);
 
-size_t eqNumbersCount(std::vector<int> rhs, int target)
-{
-    size_t counter = 0;
+bool greaterThen(int, int);
 
-    for (auto i : rhs) {
-        if (i == target) {
-            ++counter;
-        }
-    }
+size_t suiteNumbersCount(std::vector<int>, bool (int, int), int);
 
-    return counter;
-}
+void replaceNulls(std::vector<int>&);
 
-bool greaterThen(int lhs, int rhs)
-{
-    return lhs > rhs;
-}
+void intervalInput(size_t&, size_t&);
 
-size_t suiteNumbersCount(std::vector<int> rhs, bool comp(int, int), int target) 
-{
-    size_t counter = 0;
-    for (auto i : rhs) {
-        if (comp(i, target)) {
-            ++counter;
-        }
-    }
-    return counter;
-}
+void increaceByRange(std::vector<int>&);
 
-void replaceNulls(std::vector<int>& rhs)
-{
-    int average = numbersSum(rhs) / numbersCount(rhs);
-    for (auto& i : rhs) {
-        if (i == 0) {
-            i = average;
-        }
-    }
-}
+void replaceEven(std::vector<int>&);
 
-void intervalInput(size_t& begInd, size_t& endInd)
-{
-    std::cout << "Input begin index: ";
-    int temp = inputInt();
-    if (temp < 0) {
-        throw std::out_of_range("Negative index is not allowed!");
-    }
-    begInd = static_cast<size_t>(temp);
-    std::cout << "Input end index: ";
-    temp = inputInt();
-    if (temp < 0) {
-        throw std::out_of_range("Negative index is not allowed!");
-    }
-    endInd = static_cast<size_t>(temp);
-    if (begInd > endInd) {
-        throw std::logic_error("Begin index bigger than end index!");
-    }
-}
-
-void increaceByRange(std::vector<int>& rhs)
-{
-    size_t begInd = 0;
-    size_t endInd = 0;
-    intervalInput(begInd, endInd);
-    std::vector<int> range(rhs.begin() + begInd, rhs.begin() + endInd + 1);
-    int increaceValue = numbersSum(range);
-    for (auto& elem : rhs) {
-        elem += increaceValue;
-    }
-}
-
-void replaceEven(std::vector<int>& rhs)
-{
-    int min = INT_MAX;
-    int max = INT_MIN;
-
-    for (auto elem : rhs) {
-        if (elem > max) {
-            max = elem;
-        }
-        if (elem < min) {
-            min = elem;
-        }
-    }
-    
-    int minMax = max - min;
-
-    for (auto& elem : rhs) {
-        if (elem % 2 == 0) {
-            elem = minMax;
-        }
-    }
-}
-
-void deleteRepeat(std::vector<int>& rhs)
-{
-    for (size_t i = 0; i < rhs.size() - 1; ++i) {
-        for (size_t j = i+1; j < rhs.size(); ++j) {
-            if (rhs[i] == rhs[j]) {
-                rhs.erase(rhs.begin() + j);
-            }
-        }
-    }
-}
+void deleteRepeat(std::vector<int>&);
 
 #endif
