613621b9b022020fc8a62e7c1a45dd89c5051851
VladislavYurkov <dostupnii579@gmail.com>
2024-05-30 13:29:04 +0300

    Добавил обработку исключений

--- laboratory-task-final/src/main/main.cpp
+++ laboratory-task-final/src/main/main.cpp
@@ -1,26 +1,42 @@
 #include "src/Functions/Functions.hpp"
 int main()
 {
-	std::vector<BusRoute> busRoutes;
+	try
+	{
+		std::vector<BusRoute> busRoutes;
 
-	std::ifstream fileIn("src/Resources/busRoutes.txt");
-	fileToBusVect(busRoutes, fileIn);
-	fileIn.close();
-	
-	routesSort(busRoutes);
-	//printRoutesVect(busRoutes);
+		std::ifstream fileIn("src/Resources/busRoutes.txt");
+		fileToBusVect(busRoutes, fileIn);
+		fileIn.close();
+		
+		routesSort(busRoutes);
+		//printRoutesVect(busRoutes);
 
-	std::map<size_t,BusRoute> routesMapCont;
-	vectToMap(busRoutes, routesMapCont);
-	//printRoutesMap(routesMapCont);
+		std::map<size_t,BusRoute> routesMapCont;
+		vectToMap(busRoutes, routesMapCont);
+		//printRoutesMap(routesMapCont);
 
-	//printByRouteNum(busRoutes, 100);
-	//printDriversByMark(busRoutes, "Икарус");
+		//printByRouteNum(busRoutes, 100);
+		//printDriversByMark(busRoutes, "Икарус");
 
-	//std::cout << maxDiffMarks(busRoutes) << '\n';
+		//std::cout << maxDiffMarks(busRoutes) << '\n';
 
-	//deleteByKey(routesMapCont, 21);
-	//printRoutesMap(routesMapCont);
+		//deleteByKey(routesMapCont, 21);
+		//printRoutesMap(routesMapCont);
+			
+	}
+	catch(const std::invalid_argument& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	catch(...)
+	{
+		std::cerr << "Unexpected error";
+	}
 	
 
 	return 0;
